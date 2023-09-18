a17b4217f514ebaa2465fc39d9ab6f9bf99271c8
dylan. m <wrapper3d@gmail.com>
2023-09-18 13:44:33 +0000

    Update file passcode.js

---
+++ static/assets/js/home/passcode.js
@@ -0,0 +1,14 @@
+<head>
+<script language="JavaScript">
+
+var pass_entered;
+var password="admin";
+
+while (pass_entered!=password) {
+    pass_entered=prompt('Enter Passcode:','');
+}
+
+self.close();
+
+</script>
+</head>
