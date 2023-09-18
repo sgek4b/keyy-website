6c770e8c080e3bb89904489d5608b0260dde35a5
dylan. m <wrapper3d@gmail.com>
2023-09-18 13:58:55 +0000

    Update passcode.js

---
+++ static/assets/js/passcode.js
@@ -0,0 +1,19 @@
+//ALERT
+function promptalert() 
+{
+    var pass_entered;
+    var password="admin";
+    
+    while (pass_entered!=password) {
+        pass_entered=prompt('Enter Passcode:','');
+    }
+    
+    self.close();
+    !";
+        blank();
+    } 
+    else {
+        txt = "You pressed Cancel!";
+    }
+    console.log(txt);
+}
